5d9e6c80aacf502daf8d82e051647f348bc231ab
myTech.Today <mytechtoday@protonmail.com>
2023-10-18 12:43:51 -0500

    modified:   PlaylistUpdater.php

--- PlaylistUpdater.php
+++ PlaylistUpdater.php
@@ -5,6 +5,18 @@ class PlaylistUpdater {
         $playlist_data = $request->get_json_params();
         $post_id = $request['id'];
 
+        // Check if the post with the given ID exists
+        $existing_playlist = get_post($post_id);
+
+        if (!$existing_playlist) {
+            return new WP_REST_Response("Playlist with ID '{$post_id}' does not exist.", 404); // 404 Not Found
+        }
+
+        // Check if the title needs an update to avoid redundant operations
+        if ($existing_playlist->post_title === $playlist_data['title']) {
+            return new WP_REST_Response("No changes detected.", 304); // 304 Not Modified
+        }
+
         $updated = wp_update_post(array(
             'ID' => $post_id,
             'post_title' => $playlist_data['title']
@@ -14,8 +26,8 @@ class PlaylistUpdater {
             return new WP_REST_Response("Error updating playlist.", 500);
         }
 
-        // Update metadata here if needed
-        // update_post_meta($post_id, 'meta_key', $meta_value);
+        // Invalidate cache after updating the playlist
+        wp_cache_delete("playlist_{$post_id}", 'playlists');
 
         return new WP_REST_Response(array('id' => $post_id), 200);
     }

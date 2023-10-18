e8a3d6965ba546232d858a51480323134bdaee0b
myTech.Today <mytechtoday@protonmail.com>
2023-10-18 12:37:01 -0500

    modified:   PlaylistCreator.php

--- PlaylistCreator.php
+++ PlaylistCreator.php
@@ -4,6 +4,13 @@ class PlaylistCreator {
     public static function create_playlist($request) {
         $playlist_data = $request->get_json_params();
 
+        // Check if the playlist with the given title already exists
+        $existing_playlist = get_page_by_title($playlist_data['title'], OBJECT, 'playlist');
+
+        if ($existing_playlist) {
+            return new WP_REST_Response("Playlist with the title '{$playlist_data['title']}' already exists.", 409); // 409 Conflict
+        }
+
         // Create post
         $post_id = wp_insert_post(array(
             'post_title' => $playlist_data['title'],
@@ -14,6 +21,9 @@ class PlaylistCreator {
         if(!$post_id) {
             return new WP_REST_Response("Error creating playlist.", 500);
         }
+
+        // Cache the created playlist for faster subsequent access
+        wp_cache_set("playlist_{$post_id}", $playlist_data, 'playlists', 3600); // Cache for 1 hour
 
         // Add metadata here if needed
         // update_post_meta($post_id, 'meta_key', $meta_value);

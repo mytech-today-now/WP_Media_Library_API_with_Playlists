13ee8d9d323e1cdbec48e51d40f2ea580d603288
myTech.Today <mytechtoday@protonmail.com>
2023-10-18 12:41:34 -0500

    modified:   PlaylistFetcher.php

--- PlaylistFetcher.php
+++ PlaylistFetcher.php
@@ -7,7 +7,18 @@ class PlaylistFetcher {
             'posts_per_page' => -1
         );
 
+        // Try to get cached results
+        $cached_playlists = wp_cache_get('all_playlists', 'playlists');
+
+        if ($cached_playlists) {
+            return new WP_REST_Response($cached_playlists, 200);
+        }
+
         $playlists = get_posts($args);
+
+        // Cache the results for faster subsequent access
+        wp_cache_set('all_playlists', $playlists, 'playlists', 3600); // Cache for 1 hour
+
         return new WP_REST_Response($playlists, 200);
     }
 }

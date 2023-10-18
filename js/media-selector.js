4d2e526ba0361c31108b7f7d0bd1a362f4ba5a2c
myTech.Today <mytechtoday@protonmail.com>
2023-10-18 05:50:59 -0500

    modified:   js/media-selector.js

--- js/media-selector.js
+++ js/media-selector.js
@@ -1,12 +1,16 @@
 
 jQuery(document).ready(function($) {
 
+    console.log('Plugin Initialized');
+
     // Open the media library
     function open_media_library(targetInput) {
+        console.log('Opening media library');
         var custom_uploader;
 
         // If the uploader object has already been created, reopen the dialog
         if (custom_uploader) {
+            console.log('Reopening media library dialog');
             custom_uploader.open();
             return;
         }
@@ -22,6 +26,7 @@ jQuery(document).ready(function($) {
 
         // When a file is selected, grab the URL and set it as the text field's value
         custom_uploader.on('select', function() {
+            console.log('Media selected');
             var attachments = custom_uploader.state().get('selection').map(function(attachment) {
                 attachment.toJSON();
                 return attachment;
@@ -29,6 +34,7 @@ jQuery(document).ready(function($) {
 
             // Loop through selected items and perform your actions (e.g., appending to a list)
             attachments.forEach(function(attachment) {
+                console.log('Processing attachment:', attachment.attributes.url);
                 // Example: append the attachment URL to an input
                 $(targetInput).val(attachment.attributes.url);
                 // You might want to store other details, like attachment ID or title, depending on your needs
@@ -37,6 +43,7 @@ jQuery(document).ready(function($) {
 
         // Open the uploader dialog
         custom_uploader.open();
+        console.log('Media library dialog opened');
     }
 
     // Assuming you have a button with class 'open-media-library'
@@ -44,4 +51,6 @@ jQuery(document).ready(function($) {
         e.preventDefault();
         open_media_library('#target-input');  // '#target-input' is the ID of the input where you want to store the media URL
     });
+
+    console.log('Plugin Ready');
 });

a6cca726a189662bee46b52d0b7bc24d4d2dface
myTech.Today <mytechtoday@protonmail.com>
2023-10-20 15:05:31 -0500

    modified:   init.php 	modified:   main-plugin-file.php

--- init.php
+++ init.php
@@ -1,55 +1,15 @@
 <?php
 
 // Instantiate the logging class
-$bufferedLogger = new WPMediaBufferedLogger();
+//$bufferedLogger = new WPMediaBufferedLogger();
 
-$bufferedLogger->log_me("Init loaded");
+
+// $bufferedLogger->log_me("Init loaded");
 
 // Prevent direct file access
 if (!defined('ABSPATH')) {
-    $bufferedLogger->log_me("Exiting because of direct file access not defined");
+    // $bufferedLogger->log_me("Exiting because of direct file access not defined");
+    echo "Exiting because of direct file access not defined";
     exit;
-}
-
-// Check for WordPress version compatibility
-global $wp_version;
-if (version_compare($wp_version, '5.0', '<')) {
-    deactivate_plugins(basename(__FILE__)); // Deactivate the plugin
-    $bufferedLogger->log_me("This plugin requires WordPress version 5.0 or higher.");
-    wp_die("This plugin requires WordPress version 5.0 or higher.");
-}
-
-// Check for required resources
-$required_resources = ['js/script.js', 'css/style.css'];
-foreach ($required_resources as $resource) {
-    if (!file_exists(plugin_dir_path(__FILE__) . $resource)) {
-        deactivate_plugins(basename(__FILE__)); // Deactivate the plugin
-        $bufferedLogger->log_me("Missing required resource: {$resource}.");
-        wp_die("Missing required resource: {$resource}.");
-    }
-}
-
-// Include necessary files
-$files_to_include = [
-    'admin-interface.php',
-    'ajax-handlers.php',
-    'APIEndpointsRegistrar.php',
-    'install.php',
-    'playlist-renderer.php',
-    'PlaylistCreator.php',
-    'PlaylistDeleter.php',
-    'PlaylistFetcher.php',
-    'PlaylistUpdater.php',
-    'shortcode.php',
-    'uninstall.php',
-    'utils.php'
-];
-
-foreach ($files_to_include as $file) {
-    if (file_exists(plugin_dir_path(__FILE__) . $file)) {
-        require_once(plugin_dir_path(__FILE__) . $file);
-    } else {
-        $bufferedLogger->log_me("Missing file: {$file}");
-    }
 }
 ?>
--- main-plugin-file.php
+++ main-plugin-file.php
@@ -14,7 +14,7 @@
 require_once(plugin_dir_path(__FILE__) . 'config.php');
 echo "config.php loaded";
 
-
+/*
 class WPMediaBufferedLogger {
     public function __construct() {
         add_action('init', [$this, 'start_buffering']);
@@ -51,19 +51,21 @@ class WPMediaBufferedLogger {
 }
 
 $bufferedLogger = new WPMediaBufferedLogger();
-
+*/
 // Plugin Activation
 function custom_playlist_activation() {
     global $bufferedLogger;
+    echo "custom_playlist_activation";
     // Code to run on plugin activation
-    $bufferedLogger->log_me("Code to run on plugin activation - TBD");
+ //   $bufferedLogger->log_me("Code to run on plugin activation - TBD");
 }
 
 // Plugin Deactivation
 function custom_playlist_deactivation() {
     global $bufferedLogger;
+    echo "custom_playlist_deactivation";
     // Code to run on plugin deactivation
-    $bufferedLogger->log_me("Code to run on plugin deactivation - TBD");
+   // $bufferedLogger->log_me("Code to run on plugin deactivation - TBD");
 }
 
 register_activation_hook(__FILE__, 'custom_playlist_activation');
@@ -73,7 +75,8 @@ register_deactivation_hook(__FILE__, 'custom_playlist_deactivation');
 global $wp_version;
 if (version_compare($wp_version, '5.0', '<')) {
     deactivate_plugins(plugin_basename(__FILE__)); // Deactivate the plugin
-    $bufferedLogger->log_me("This plugin requires WordPress version 5.0 or higher.");
+    //$bufferedLogger->log_me("This plugin requires WordPress version 5.0 or higher.");
+    echo "This plugin requires WordPress version 5.0 or higher.";
     wp_die("This plugin requires WordPress version 5.0 or higher.");
 }
 
@@ -82,7 +85,8 @@ $required_resources = ['js/script.js', 'css/style.css'];
 foreach ($required_resources as $resource) {
     if (!file_exists(plugin_dir_path(__FILE__) . $resource)) {
         deactivate_plugins(basename(__FILE__)); // Deactivate the plugin
-        $bufferedLogger->log_me("Missing required resource: {$resource}.");
+        //$bufferedLogger->log_me("Missing required resource: {$resource}.");
+        echo "Missing required resource: {$resource}";
         wp_die("Missing required resource: {$resource}.");
     }
 }
@@ -109,7 +113,8 @@ foreach ($files_to_include as $file) {
     if (file_exists(plugin_dir_path(__FILE__) . $file)) {
         require_once(plugin_dir_path(__FILE__) . $file);
     } else {
-        $bufferedLogger->log_me("Missing file: {$file}");
+        echo "Missing file: {$file}";
+        //$bufferedLogger->log_me("Missing file: {$file}");
     }
 }
 ?>

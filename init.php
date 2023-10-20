e45c3e4b19dfd35095ddeff470fc5ba412cffaf8
myTech.Today <mytechtoday@protonmail.com>
2023-10-20 11:41:23 -0500

    modified:   init.php

--- init.php
+++ init.php
@@ -3,6 +3,8 @@
 // Instantiate the logging class
 $bufferedLogger = new WPMediaBufferedLogger();
 
+$bufferedLogger->log_me("Init loaded");
+
 // Prevent direct file access
 if (!defined('ABSPATH')) {
     $bufferedLogger->log_me("Exiting because of direct file access not defined");
